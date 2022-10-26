b6041bb4251fc90d045cfbfc273517b4293037f8
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-10-26 20:11:08 +0300

    Makes App feedback ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,76 @@
-import Feedback from './feedback';
+import React, { Component } from 'react';
+import {
+  FirstText,
+  Container,
+  BtnContainer,
+  Button,
+  StatisticsText,
+  StatisticsItem,
+} from './App.styled';
 
-export const App = () => {
-  return <Feedback />;
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleGoodIncrement = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+
+  handleNeutralIncrement = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+
+  handleBadIncrement = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  render() {
+    const totalVotes = this.state.good + this.state.neutral + this.state.bad;
+    const positiveFeedbackPercentage =
+      (this.state.good / totalVotes) * 100 || 0;
+
+    return (
+      <Container>
+        <FirstText>Please leave feedback</FirstText>
+        <BtnContainer>
+          <Button type="button" onClick={this.handleGoodIncrement}>
+            Good
+          </Button>
+          <Button type="button" onClick={this.handleNeutralIncrement}>
+            Neutral
+          </Button>
+          <Button type="button" onClick={this.handleBadIncrement}>
+            Bad
+          </Button>
+        </BtnContainer>
+        <StatisticsText>Statistics</StatisticsText>
+        <ul>
+          <StatisticsItem>Good: {this.state.good}</StatisticsItem>
+          <StatisticsItem>Neutral: {this.state.neutral}</StatisticsItem>
+          <StatisticsItem>Bad: {this.state.bad}</StatisticsItem>
+          <StatisticsItem>Total: {totalVotes}</StatisticsItem>
+          <StatisticsItem>
+            Positive feedback: {positiveFeedbackPercentage.toFixed()}%
+          </StatisticsItem>
+        </ul>
+      </Container>
+    );
+  }
+}
+
+export default App;
